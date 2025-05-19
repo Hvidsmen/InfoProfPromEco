0b2f40e0001e64e47d83f61c514c098e33ed9daf
hvids <hvidsmen@mail.ru>
2025-05-19 12:16:34 +0300

    Menu

--- header.php
+++ header.php
@@ -1,97 +1,103 @@
 <div class="container bg-light rounded-1">
     <div class="row">
+        <div class="col-1  align-self-center">
+            <button class="btn btn-light ">
+                <a href="./index.php" class="link-dark link-no-underline">Главная</a>
+            </button>
+        </div>
+        <div class="col-3  align-self-center">
+            <button class="btn btn-light">
+                <a href="./documents.php" class="link-dark link-no-underline">Сведения об образовательной
+                    организации</a>
+            </button>
+        </div>
+        <div class="col-3  align-self-center">
+            <button class="btn btn-light">
+                <a href="cources.php" class="link-dark link-no-underline text-start"> Программы повышения
+                    квалификации</a>
+            </button>
+        </div>
+        <div class="col-1  align-self-center">
+            <button class="btn btn-light">
+                <a href="./uslugs.php" class="link-dark link-no-underline">Услуги</a>
+            </button>
+        </div>
+        <div class="col-1  align-self-center">
+            <button class="btn btn-light">
+                <a href="contacts.php" class="link-dark link-no-underline">Контакты</a>
+            </button>
+        </div>
+        <div class="col-1  align-self-center">
+            <button class="btn btn-light">
+                <a href="https://p30742.edu.riskprof.ru/#/" class="link-dark link-no-underline">СДО</a>
+            </button>
+        </div>
         <div class="col-2  align-self-center">
-            <img src="./img/logotip.webp" class="center-block" width="100%" height="100%">
-        </div>
-        <div class="col">
-            <div class="row">
-                <div class="col text-start align-self-center">
-                    <h1 class="text-start">ООО «ПрофПромЭко»</h1>
-                    <p class="text-start fs-2">Дополнительно образование</p>
-                </div>
-                <div class="row">
-                    <div class="col  align-self-center">
-                        <button class="btn btn-light ">
-                            <a href="./index.php" class="link-dark link-no-underline">Главная</a>
-                        </button>
-                        <br>
-                        <button class="btn btn-light text-start">
-                            <a href="./documents.php" class="link-dark link-no-underline">Сведения об образовательной организации</a>
-                        </button>
-                    </div>
-                    <div class="col  align-self-center">
-                        <button class="btn btn-light text-start">
-                            <a href="cources.php" class="link-dark link-no-underline text-start"> Программы повышения квалификации</a>
-                        </button>
-                        <br>
-                        <button class="btn btn-light">
-                            <a href="./uslugs.php" class="link-dark link-no-underline">Услуги</a>
-                        </button>
-
-                    </div>
-                    <div class="col  align-self-center text-start">
-                        <button class="btn btn-light">
-                            <a href="contacts.php" class="link-dark link-no-underline">Контакты</a>
-                        </button>
-                        <br>
-                        <button class="btn btn-light">
-                            <a href="https://p30742.edu.riskprof.ru/#/" class="link-dark link-no-underline">СДО</a>
-                        </button>
-                        <button class="btn btn-light">
-                            <a href="https://iprofpromeducation.pro/list_course/" class="link-dark link-no-underline">ПРОФ СДО</a>
-                        </button>
-
-
-                    </div>
-
-                    <div class="col-4  align-self-center text-center">
-                        <div class="row align-items-start">
-                            <div class="col-1 "><img src="img/free-icon-home-phone-17280761.png" class="mx-auto d-block"
-                                                     width="20px" height="20px"></div>
-                            <div class="col text-start"><h6>+7 (499)-755-52-05</h6></div>
-                        </div>
-                        <div class="row align-items-start">
-                            <div class="col-1 text-end"><img src="img/telephone.svg" class="mx-auto d-block"></div>
-                            <div class="col text-start"><h6>+7 (925)-230-20-55</h6></div>
-                        </div>
-                        <div class="row align-items-start">
-                            <div class="col-1"><img src="img/envelope.svg" class="mx-auto d-block"></div>
-                            <div class="col text-start"><h6>info@profpromeco.pro</h6></div>
-                        </div>
-                    </div>
-                    <div class="col">
-
-                        <a id="specialButton" href="#" class="btn btn-primary text-center">
-                            <h3>Версия для слабовидящих</h3>
-                        </a>
-                        <style>
-                            @media screen and (max-width: 980px) {
-                                #glazd {
-                                    position: absolute;
-                                    top: 10px;
-                                    right: 100px;
-                                    z-index: 99999;
-                                    width: 50px;
-                                }
-                            }
-
-                            @media screen and (min-width: 980px) {
-                                #glazd {
-                                    position: absolute;
-                                    top: 20px;
-                                    left: 50px;
-                                    z-index: 99999;
-                                    width: 50px;
-                                }
-                            }
-
-                        </style
-                        >
-                    </div>
-                </div>
-            </div>
+            <button class="btn btn-light">
+                <a href="https://iprofpromeducation.pro/list_course/" class="link-dark link-no-underline">ПРОФ СДО</a>
+            </button>
         </div>
 
+    </div>
+    <div class="row">
 
     </div>
 </div>
+<br>
+<div class="container bg-light rounded-1">
+    <div class="row">
+        <div class="col-2  align-self-center">
+            <img src="./img/logotip.webp" class="center-block" width="70%" height="70%">
+        </div>
+        <div class="col align-self-center text-center">
+            <h2 class="text-start">ООО «ПрофПромЭко»</h2>
+            <p class="text-start fs-2">Дополнительно образование</p>
+        </div>
+        <div class="col-3  align-self-center text-center">
+            <div class="container">
+                <div class="row align-items-start">
+                    <div class="col-1 "><img src="img/free-icon-home-phone-17280761.png" class="mx-auto d-block"
+                                             width="20px" height="20px"></div>
+                    <div class="col text-start"><h6>+7 (499)-755-52-05</h6></div>
+                </div>
+                <div class="row align-items-start">
+                    <div class="col-1 text-end"><img src="img/telephone.svg" class="mx-auto d-block"></div>
+                    <div class="col text-start"><h6>+7 (925)-230-20-55</h6></div>
+                </div>
+                <div class="row align-items-start">
+                    <div class="col-1"><img src="img/envelope.svg" class="mx-auto d-block"></div>
+                    <div class="col text-start"><h6>info@profpromeco.pro</h6></div>
+                </div>
+            </div>
+        </div>
+        <div class="col-3 align-self-center text-center">
+            <a id="specialButton" href="#" class="btn btn-primary text-center">
+                <h3>Версия для слабовидящих</h3>
+            </a>
+            <style>
+                @media screen and (max-width: 980px) {
+                    #glazd {
+                        position: absolute;
+                        top: 10px;
+                        right: 100px;
+                        z-index: 99999;
+                        width: 50px;
+                    }
+                }
+
+                @media screen and (min-width: 980px) {
+                    #glazd {
+                        position: absolute;
+                        top: 20px;
+                        left: 50px;
+                        z-index: 99999;
+                        width: 50px;
+                    }
+                }
+
+            </style
+            >
+        </div>
+    </div>
+</div>
+
